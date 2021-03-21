11b2a6a64a4955b892de155514d3ee58dda6777e
ARYANTECH123 <ateamxxx101@gmail.com>
2021-03-21 18:02:15 +0530

    Added automatic loading of tasks from the tasks array in tasklodaer.js

--- static/scripts/taskloader.js
+++ static/scripts/taskloader.js
@@ -1,12 +1,17 @@
 tasks = [
     {name:"Ayush", task:"task1", status:"todo"},
     {name:"Ayush", task:"task2", status:"inprogress"},
+    {name:"Ayush", task:"task2.1", status:"inprogress"},
     {name:"Ayush", task:"task3", status:"done"}
 
 ];
 
 function loadTasks() {
-    for(entry in tasks){
-        console.log(entry);
+    for(i = 0; i < tasks.length; i++){
+        entry = tasks[i];
+        output = `<div key="${i}">${entry.name}: ${entry.task}</div>`;
+
+        document.getElementById(entry.status).innerHTML += output;
     }
+
 }
